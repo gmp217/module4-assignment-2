4abf23958f0c36daefb350a1c9a68cafdc193a78
gmp217 <66996033+gmp217@users.noreply.github.com>
2020-06-29 22:46:49 +0530

    Update 321810304046-unique elements.py

--- 321810304046-unique elements.py
+++ 321810304046-unique elements.py
@@ -1,10 +1,16 @@
-ul,l=[],[]
-n=int(input('Enter no of elements: '))
-print('Enter elememts: ')
-for i in range(0,n):
-	ele=input()
-	l.append(ele)
-for e in l:
-	if e not in ul:
-		ul.append(e)
-print('unique list:',ul)
+l,n,i,c=[],int(input("Enter no of elements : ")),0,0 
+for t in range(0, n):
+	e= int(input()) 
+	l.append(e)
+print("unique elements are:")
+while i < len(l):
+    j=0
+    while j < len(l):
+     if l[i]==l[j] and i!=j:
+      c+=1
+      break
+     j+=1
+    if c==0:
+     print(l[i])
+    i+=1
+    c=0    
